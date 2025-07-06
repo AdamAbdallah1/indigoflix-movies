8190ee805af4c16a1a8c3f609d83c3f4eb02a949
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-06 23:30:27 +0300

    Initailizing switch

---
+++ src/assets/components/Switch.jsx
@@ -0,0 +1,10 @@
+function Switch( {value} ) {
+    return(
+        <div className="text-gradient text-2xl flex flex-row">
+            <button className="bg-[030014] cursor-pointer">Movies</button>
+            <button className="bg-[030014] cursor-pointer">Series</button>
+        </div>
+    )
+}
+
+export default Switch
