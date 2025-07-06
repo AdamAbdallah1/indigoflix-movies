7ecf20de36cfc2467f8a9b5e128b6d52fa2bd54e
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-06 23:32:47 +0300

    Creating buttons container

--- src/assets/components/Switch.jsx
+++ src/assets/components/Switch.jsx
@@ -1,6 +1,6 @@
-function Switch( {value} ) {
+function Switch() {
     return(
-        <div className="text-gradient text-2xl flex flex-row">
+        <div className="text-gradient text-2xl flex justify-evenly p-4">
             <button className="bg-[030014] cursor-pointer">Movies</button>
             <button className="bg-[030014] cursor-pointer">Series</button>
         </div>
