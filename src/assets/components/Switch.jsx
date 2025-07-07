44671ee7b7eebdb03e6e924a144227804ef75b27
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-07 18:37:37 +0300

    Updating to take type and setType props

--- src/assets/components/Switch.jsx
+++ src/assets/components/Switch.jsx
@@ -1,10 +1,24 @@
-function Switch() {
-    return(
-        <div className="text-gradient text-2xl flex justify-evenly p-4 mt-[-70px]">
-            <button className="bg-[030014] cursor-pointer">Movies</button>
-            <button className="bg-[030014] cursor-pointer">Series</button>
-        </div>
-    )
+function Switch({ type, setType }) {
+  return (
+    <div className="text-gradient text-2xl flex justify-evenly p-4 mt-[-70px]">
+      <button
+        onClick={() => setType('movie')}
+        className={`px-4 py-2 cursor-pointer ${
+          type === 'movie' ? 'border-b-2 border-white' : ''
+        }`}
+      >
+        Movies
+      </button>
+      <button
+        onClick={() => setType('tv')}
+        className={`px-4 py-2 cursor-pointer ${
+          type === 'tv' ? 'border-b-2 border-white' : ''
+        }`}
+      >
+        Series
+      </button>
+    </div>
+  )
 }
 
 export default Switch
