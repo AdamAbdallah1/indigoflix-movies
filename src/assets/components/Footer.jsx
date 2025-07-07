44f82ecc096a8b0180f8552b832c04b1a5760e47
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-07 19:58:45 +0300

    Adding full name

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="bg-dark-100 text-gray-400 text-center py-4">
             <p className="text-sm">
-                &copy; {new Date().getFullYear()} Adam — All rights reserved.
+                &copy; {new Date().getFullYear()} Adam Abdallah — All rights reserved.
             </p>
         </footer>
     )
